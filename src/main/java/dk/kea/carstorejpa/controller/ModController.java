78733f7c3f8ef260e527b6b15f5a7559bd7ff24d
jarltuxen <jarl.tuxen@gmail.com>
2021-04-22 11:25:56 +0200

    controller

--- src/main/java/dk/kea/carstorejpa/controller/ModController.java
+++ src/main/java/dk/kea/carstorejpa/controller/ModController.java
@@ -21,38 +21,43 @@ public class ModController {
     @Autowired
     BrandService brandService;
 
-    @Autowired
-    ModRepository modRepository;
-
     @GetMapping("/mods")
     public String mods(Model model){
+        model.addAttribute("mods", modService.findAll());
         return "mods";
     }
 
     @GetMapping("/create")
     public String create(Model model){
+        //tilføj liste af brands, så der kan vælges brand
+        model.addAttribute("brands", brandService.findAll());
         return "create";
     }
 
     @PostMapping("/create")
     public String create(@ModelAttribute Mod mod){
+        modService.create(mod);
         return "redirect:/mods";
     }
 
     @GetMapping("/update/{id}")
     public String update(@PathVariable("id") long id, Model modelMods, Model modelBrands){
+        modelMods.addAttribute("mods", modService.findById(id));
         //add brands to choose from
+        modelBrands.addAttribute("brands", brandService.findAll());
         return "update";
     }
 
     @PostMapping("/update")
     public String update(@ModelAttribute Mod mod){
         //update by using update service
+        modService.update(mod);
         return "redirect:/mods";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable("id") long id, Model model){
+        modService.deleteById(id);
         return "redirect:/mods";
     }
 
