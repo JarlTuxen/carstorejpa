4cb0ceb0bf44cc7e94fd6514bd38d93bf683dda0
jarltuxen <jarl.tuxen@gmail.com>
2021-04-29 10:24:12 +0200

    note controller - onetoone create bidirectionalt

--- src/main/java/dk/kea/carstorejpa/controller/ModController.java
+++ src/main/java/dk/kea/carstorejpa/controller/ModController.java
@@ -1,8 +1,10 @@
 package dk.kea.carstorejpa.controller;
 
 import dk.kea.carstorejpa.model.Mod;
+import dk.kea.carstorejpa.model.Note;
 import dk.kea.carstorejpa.service.BrandService;
 import dk.kea.carstorejpa.service.ModService;
+import dk.kea.carstorejpa.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,9 @@ public class ModController {
     @Autowired
     BrandService brandService;
 
+    @Autowired
+    NoteService noteService;
+
     @GetMapping("/mods")
     public String mods(Model model){
         model.addAttribute("mods", modService.findAll());
@@ -35,7 +40,16 @@ public class ModController {
 
     @PostMapping("/create")
     public String create(@ModelAttribute Mod mod){
-        modService.create(mod);
+        Mod newMod = modService.create(mod);
+
+        //opret ny note med newMod som reference
+        Note note = new Note("tilføj noter", newMod);
+        noteService.create(note);
+
+        //sæt reference til note i newMod
+        newMod.setNote(note);
+        modService.update(newMod);
+
         return "redirect:/mods";
     }
 
@@ -57,6 +71,19 @@ public class ModController {
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable("id") long id, Model model){
         modService.deleteById(id);
+        return "redirect:/mods";
+    }
+
+    @GetMapping("/updateNote/{id}")
+    public String updateNote(@PathVariable("id") long id, Model model){
+        model.addAttribute("note", noteService.findById(id));
+        return "updateNote";
+    }
+
+    @PostMapping("/updateNote")
+    public String updateNote(@ModelAttribute Note note){
+        //update by using update service
+        noteService.update(note);
         return "redirect:/mods";
     }
 
